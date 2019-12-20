8835329a19e671d559b1130ba23f7f1a7153e58e
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-20 10:42:56 +0700

    startActivity

--- app/src/main/java/com/numfa/androidlayouts/MainActivity.kt
+++ app/src/main/java/com/numfa/androidlayouts/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.numfa.androidlayouts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        b1.setOnClickListener {
+            val i = Intent(this, LinearActivity::class.java)
+            startActivity(i)
+        }
+        b2.setOnClickListener {
+            val i = Intent(this, LinearActivity::class.java)
+            startActivity(i)
+        }
+        b3.setOnClickListener {
+            val i = Intent(this, LinearActivity::class.java)
+            startActivity(i)
+        }
     }
 }
