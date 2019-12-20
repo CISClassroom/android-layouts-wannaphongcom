f594c10c722622319854da35573161707272c139
Wannaphong Phatthiyaphaibun <wannaphong@yahoo.com>
2019-12-20 11:54:37 +0700

    update code

--- app/src/main/java/com/numfa/androidlayouts/ConstraintActivity.kt
+++ app/src/main/java/com/numfa/androidlayouts/ConstraintActivity.kt
@@ -2,11 +2,15 @@ package com.numfa.androidlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_constraint.*
 
 class ConstraintActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constraint)
+        bb.setOnClickListener {
+            this.finish() // หยุดการทำงาน Activity
+        }
     }
 }
